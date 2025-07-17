f1144f7f96bae00bc66afa3ddc9903d4221beed7
bluesky-520 <oscar12312390@outlook.com>
2025-07-17 02:31:47 -0400

    add user information

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  // Get the userId from auth() -- if null, the user is not signed in
+  const { userId } = await auth();
+
+  // Protect the route by checking if the user is signed in
+  if (!userId) {
+    return <div>Sign in to view this page</div>
+  }
+
+  // Get the Backend API User object when you need access to the user's information
+  const user = await currentUser();
+
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {/* Use `user` to render user details or create UI elements */}
+        <h1>Welcome {user?.firstName} {user?.lastName}</h1>
         <Image
           className="dark:invert"
           src="/next.svg"
